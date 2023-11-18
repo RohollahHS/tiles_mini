c5d440d7b42c2552e84d43430aff3ca312ac7190
Rohollah <rohollah903@gmail.com>
2023-11-18 11:24:19 +0330

    Ready to train

--- make_env.sh
+++ make_env.sh
@@ -4,9 +4,9 @@ mkdir outputs
 
 gdown '1-89C2dX4pZEazjGa3JmIJ42RpoVReY4C&confirm=True'
 mv tiles_mini.zip data
-rm data/tiles_mini.zip
 unzip data/tiles_mini.zip -d data/
 rm -r 'data/content'
+rm data/tiles_mini.zip
 
 wget 'https://download.pytorch.org/models/resnet50-11ad3fa6.pth'
 mv resnet50-11ad3fa6.pth checkpoints/
